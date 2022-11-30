347a0d881675cf4710e1937001339c1920ef6b13
Galex Yen <galexy@users.noreply.github.com>
2022-11-30 10:31:24 -0800

    Refactor log so that it can start from a non-zero index

--- src/raft/log.go
+++ src/raft/log.go
@@ -18,22 +18,24 @@ func (l *LogEntry) String() string {
 type Log interface {
 	append(term Term, command interface{}) (newEntry, prevEntry *LogEntry)
 	hasEntryAt(index LogIndex, term Term) (hasPrevEntry bool, conflictTerm Term, conflictTermStartIndex LogIndex)
+	getEntryAt(index LogIndex) (entry *LogEntry)
 	getEntriesFrom(index LogIndex) (prevEntry *LogEntry, entries []*LogEntry)
 	nextIndex() LogIndex
 	insertReplicatedEntries(entries []*LogEntry)
 	lastLogEntry() (index LogIndex, term Term)
-	getEntryAt(index LogIndex) (entry *LogEntry)
 	save(encoder *labgob.LabEncoder) error
 	load(encoder *labgob.LabDecoder) error
 }
 
 type LogImpl struct {
-	entries []*LogEntry // Entries in the current server's log
-	rf      *Raft       // Link to main raft
+	startIndex LogIndex    // What is the latest index in the snapshot. This index represents 0 in slide
+	entries    []*LogEntry // Slice holding active log entries, 1-based indexing with 0 holds sentinel
+	rf         *Raft       // Link to main raft
 }
 
 func MakeLog(rf *Raft) Log {
 	log := &LogImpl{rf: rf}
+	log.startIndex = LogIndex(0)
 	log.entries = make([]*LogEntry, 1, 1024)
 	log.entries[0] = &LogEntry{Index: 0, Term: 0, Command: nil}
 
@@ -42,9 +44,11 @@ func MakeLog(rf *Raft) Log {
 
 func (l *LogImpl) append(term Term, command interface{}) (newEntry, prevEntry *LogEntry) {
 	newIndex := l.nextIndex()
-	if newIndex > 0 {
-		prevEntry = l.entries[newIndex-1]
+	if newIndex <= 0 {
+		panic("New index should be at least 1.")
 	}
+
+	prevEntry = l.getEntryAt(newIndex - 1)
 	newEntry = &LogEntry{Index: newIndex, Term: term, Command: command}
 	l.entries = append(l.entries, newEntry)
 	l.rf.persist()
@@ -53,11 +57,11 @@ func (l *LogImpl) append(term Term, command interface{}) (newEntry, prevEntry *L
 }
 
 func (l *LogImpl) hasEntryAt(index LogIndex, term Term) (hasEntry bool, conflictTerm Term, conflictTermStartIndex LogIndex) {
-	if len(l.entries) <= int(index) {
+	if l.startIndex+LogIndex(len(l.entries)) <= index {
 		panic("Not expected")
 	}
 
-	entryAtIndex := l.entries[index]
+	entryAtIndex := l.getEntryAt(index)
 	if entryAtIndex.Term == term {
 		hasEntry = true
 		return
@@ -72,27 +76,38 @@ func (l *LogImpl) hasEntryAt(index LogIndex, term Term) (hasEntry bool, conflict
 func (l *LogImpl) findTermStart(entry *LogEntry) (startIndex LogIndex) {
 	// TODO: build an map to find these index positions in O(1) time
 	startIndex = entry.Index
-	for ; startIndex > 0 && l.entries[startIndex].Term == entry.Term; startIndex-- {
+	for ; startIndex > 0 && l.getEntryAt(startIndex).Term == entry.Term; startIndex-- {
 	}
 	startIndex = startIndex + 1
 	return startIndex
 }
 
+func (l *LogImpl) getEntryAt(index LogIndex) (entry *LogEntry) {
+	effectiveIndex := index - l.startIndex
+	return l.entries[effectiveIndex]
+}
+
 func (l *LogImpl) getEntriesFrom(index LogIndex) (prevEntry *LogEntry, entries []*LogEntry) {
-	prevIndex := index - 1
+	effectiveIndex := index - l.startIndex
+	prevIndex := effectiveIndex - 1
 	if prevIndex >= 0 {
 		prevEntry = l.entries[prevIndex]
 	}
 
-	if int(index) < len(l.entries) {
-		entries = l.entries[index:]
+	if int(effectiveIndex) < len(l.entries) {
+		entries = l.entries[int(effectiveIndex):]
 	}
 
 	return
 }
 
+func (l *LogImpl) truncateLogAt(index LogIndex) {
+	effectiveIndex := index - l.startIndex
+	l.entries = l.entries[:effectiveIndex]
+}
+
 func (l *LogImpl) nextIndex() LogIndex {
-	return LogIndex(len(l.entries))
+	return l.startIndex + LogIndex(len(l.entries))
 }
 
 func (l *LogImpl) insertReplicatedEntries(entries []*LogEntry) {
@@ -103,40 +118,36 @@ func (l *LogImpl) insertReplicatedEntries(entries []*LogEntry) {
 			panic(fmt.Sprintf("Unexpected replicated entry @%d, expecting %d.", entry.Index, nextIndex))
 		}
 
-		// case where new entry is appended to end of log
-		if entry.Index == nextIndex {
-			// Append new entry
-			DPrintf(l.rf.me, cmpLogger, "Appending replicated entry %d@T%d", entry.Index, entry.Term)
-			l.entries = append(l.entries, entry)
-			continue
-		}
-
 		// entry index already in log, check if there is a conflict
-		existingEntry := l.entries[entry.Index]
-		if existingEntry.Term != entry.Term {
-			DPrintf(l.rf.me, cmpLogger, "Conflict detected %d@T%d != replicated entry %d@T%d. Deleting entry and all that follow.",
+		if entry.Index < nextIndex {
+			existingEntry := l.getEntryAt(entry.Index)
+			if existingEntry.Term == entry.Term {
+				DPrintf(l.rf.me, cmpLogger, "Already have %d@T%d, skipping.", entry.Index, entry.Term)
+			}
+
+			DPrintf(l.rf.me, cmpLogger, "Conflict detected %d@T%d != replicated entry %d@T%d. Truncating log.",
 				existingEntry.Index, existingEntry.Term, entry.Index, entry.Term)
-			// TODO: add truncation to persistence layer
-			l.entries = l.entries[:entry.Index]
-			l.entries = append(l.entries, entry)
+
+			l.truncateLogAt(entry.Index)
 		}
 
-		DPrintf(l.rf.me, cmpLogger, "Already have %d@T%d, skipping.", entry.Index, entry.Term)
+		DPrintf(l.rf.me, cmpLogger, "Appending replicated entry %d@T%d", entry.Index, entry.Term)
+		l.entries = append(l.entries, entry)
 	}
 
 	l.rf.persist()
 }
 
 func (l *LogImpl) lastLogEntry() (index LogIndex, term Term) {
-	lastEntry := l.entries[len(l.entries)-1]
+	lastEntry := l.getEntryAt(l.nextIndex() - 1)
 	return lastEntry.Index, lastEntry.Term
 }
 
-func (l *LogImpl) getEntryAt(index LogIndex) (entry *LogEntry) {
-	return l.entries[index]
-}
-
 func (l *LogImpl) save(encoder *labgob.LabEncoder) error {
+	if err := encoder.Encode(int(l.startIndex)); err != nil {
+		return fmt.Errorf("failed to encode log start index %v", err)
+	}
+
 	numEntries := len(l.entries) - 1
 	if err := encoder.Encode(numEntries); err != nil {
 		return fmt.Errorf("failed to encode num of log entries %v", err)
@@ -158,6 +169,12 @@ func (l *LogImpl) save(encoder *labgob.LabEncoder) error {
 }
 
 func (l *LogImpl) load(decoder *labgob.LabDecoder) error {
+	var savedStartIndex int
+	if err := decoder.Decode(&savedStartIndex); err != nil {
+		return fmt.Errorf("failed to decode log start index %v", err)
+	}
+	l.startIndex = LogIndex(savedStartIndex)
+
 	var savedNumEntries int
 	if err := decoder.Decode(&savedNumEntries); err != nil {
 		return fmt.Errorf("failed to decode number of log entries %v", err)
